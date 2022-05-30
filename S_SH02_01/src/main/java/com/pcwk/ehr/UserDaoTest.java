74a8c0fa9095456a6aa0028d37fd9452b2b3199f
DH <chaewon1130@naver.com>
2022-05-30 09:02:33 +0900

    spring

---
+++ S_SH02_01/src/main/java/com/pcwk/ehr/UserDaoTest.java
@@ -0,0 +1,57 @@
+package com.pcwk.ehr;
+
+import java.sql.SQLException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class UserDaoTest {
+	
+	static final Logger LOG = LogManager.getLogger(UserDaoTest.class);
+	
+	UserDao dao;
+	UserVO userVo;
+	public UserDaoTest() {
+		dao = new UserDao();
+		userVo = new UserVO("p03", "김동호", "1130");
+	}
+	
+	public void get() throws SQLException, ClassNotFoundException{
+		UserVO outVO = dao.get(userVo);
+		if(outVO != null) {
+			LOG.debug("========================");
+			LOG.debug("=성공=");
+			LOG.debug("========================");
+		}else {
+			LOG.debug("========================");
+			LOG.debug("=실패=");
+			LOG.debug("========================");
+		}
+	}
+	
+	public void add() throws SQLException, ClassNotFoundException{
+		int flag = dao.add(userVo);
+		if(flag == 1) {
+			LOG.debug("========================");
+			LOG.debug("=성공=");
+			LOG.debug("========================");
+		}else {
+			LOG.debug("========================");
+			LOG.debug("=실패=");
+			LOG.debug("========================");
+		}
+	}
+	
+	public static void main(String[] args) {
+		UserDaoTest main = new UserDaoTest();
+		try {
+			main.add();
+			main.get();
+		} catch (ClassNotFoundException | SQLException e) {
+			LOG.debug("========================");
+			LOG.debug("=ClassNotFoundException | SQLException=" + e.getMessage());
+			LOG.debug("========================");
+			e.printStackTrace();
+		}
+	}
+}
